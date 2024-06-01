8f0610aaf6f13b9b940f299a2c66bc192c24361c
Rohit Kumar <99486732+Princerey@users.noreply.github.com>
2024-06-01 18:13:31 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (42.17%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.cpp
+++ 3379-score-of-a-string/score-of-a-string.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     int scoreOfString(string s) {
-        int n = s.length();
         int sum=0;
-        for(int i=1;i<n;i++)
-        {
+        for(int i=1;i<s.length();i++)
         sum+=abs(int(s[i]) - int(s[i-1]));
-        }
         return sum;
     }
 };
