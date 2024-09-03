36708f3363e9a5704bb68afacfc75b92a9e2c334
Rohit Kumar <99486732+Princerey@users.noreply.github.com>
2024-09-03 10:19:40 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (77.66%) - LeetSync

---
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.cpp
@@ -0,0 +1,27 @@
+class Solution {
+private:
+    int helper(int number){
+        int ans = 0;
+        while(number){
+            ans += (number % 10);
+            number /= 10;
+        }
+        return ans;
+    }
+public:
+    int getLucky(string s, int k) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+        k--;
+        int sumVal = 0;
+        for(auto &ch : s){
+            sumVal += helper((ch - 'a') + 1);
+        }
+        while(k > 0){
+            sumVal = helper(sumVal);
+            k--;
+        }
+        return sumVal;
+    }
+};
